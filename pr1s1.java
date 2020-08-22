acac8b00dc6831998ec1b6ccf154db09767ff27c
Jean C <jeankporti17@gmail.com>
2020-08-22 15:39:01 -0500

    pr1

---
+++ pr1s1.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class pr1s1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class pr1s1 extends World
+{
+
+    /**
+     * Constructor for objects of class pr1s1.
+     * 
+     */
+    public pr1s1()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
