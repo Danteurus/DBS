be7e988f4cdd70deee1d1e375415c3b9674f2e1c
Cristian Guillermo Serrano Acevedo <cristiangserrano@gmail.com>
2020-08-22 17:45:33 -0500

    Se agregó la opción de la sección 3

---
+++ s2.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class s2 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class s2 extends Actor
+{
+    /**
+     * Act - do whatever the s2 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
