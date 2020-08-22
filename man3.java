470cdf7e3a6104c50882e80da6c9c6a286dbf579
Cristian Guillermo Serrano Acevedo <cristiangserrano@gmail.com>
2020-08-22 15:02:25 -0500

    Ajuste de resolución

--- man3.java
+++ man3.java
@@ -16,6 +16,6 @@ public class man3 extends World
     public man3()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(1280, 720, 1); 
     }
 }
--- man4.java
+++ man4.java
@@ -16,6 +16,6 @@ public class man4 extends World
     public man4()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(1280, 720, 1); 
     }
 }
