5fde200e30903fc62306d035b6504aade331a943
Cristian Guillermo Serrano Acevedo <cristiangserrano@gmail.com>
2020-08-23 11:07:57 -0500

    Agregación de la flechas

---
+++ l.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class l here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class l extends Actor
+{
+    /**
+     * Act - do whatever the l wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        changeWorld();
+    }
+    
+    public void changeWorld()
+    {
+        
+    }
+}
