32d7765e70a592f0033a1ab37de57b40d59dcc10
Jean C <jeankporti17@gmail.com>
2020-08-23 20:17:23 -0500

    escenarios c y i

---
+++ correct.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class correct here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class correct extends World
+{
+
+    /**
+     * Constructor for objects of class correct.
+     * 
+     */
+    public correct()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
