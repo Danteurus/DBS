aa9cb3afc3f18e196f4d889ab181dc97844c0b77
Jean C <jeankporti17@gmail.com>
2020-09-15 06:57:55 -0500

    imagenes

---
+++ man21.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class man21 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class man21 extends World
+{
+
+    /**
+     * Constructor for objects of class man21.
+     * 
+     */
+    public man21()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1280, 720, 1);
+    }
+}
---
+++ man41.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class man41 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class man41 extends World
+{
+
+    /**
+     * Constructor for objects of class man41.
+     * 
+     */
+    public man41()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
