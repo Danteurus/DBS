b450f311921dc937b7a5497594557f6981768d3d
Jean C <jeankporti17@gmail.com>
2020-09-15 07:31:23 -0500

    cambios flechas

--- l.java
+++ l.java
@@ -45,6 +45,54 @@ public class l extends Actor
             {
                 Greenfoot.setWorld(new man5());
             }
+            if(getWorld() instanceof man21)
+            {
+                Greenfoot.setWorld(new man6());
+            }
+            if(getWorld() instanceof man22)
+            {
+                Greenfoot.setWorld(new man21());
+            }
+            if(getWorld() instanceof man23)
+            {
+                Greenfoot.setWorld(new man22());
+            }
+            if(getWorld() instanceof man24)
+            {
+                Greenfoot.setWorld(new man23());
+            }
+            if(getWorld() instanceof man31)
+            {
+                Greenfoot.setWorld(new man24());
+            }
+            if(getWorld() instanceof man32)
+            {
+                Greenfoot.setWorld(new man31());
+            }
+            if(getWorld() instanceof man33)
+            {
+                Greenfoot.setWorld(new man32());
+            }
+            if(getWorld() instanceof man41)
+            {
+                Greenfoot.setWorld(new man33());
+            }
+            if(getWorld() instanceof man42)
+            {
+                Greenfoot.setWorld(new man41());
+            }
+            if(getWorld() instanceof man43)
+            {
+                Greenfoot.setWorld(new man42());
+            }
+            if(getWorld() instanceof man51)
+            {
+                Greenfoot.setWorld(new man43());
+            }
+            if(getWorld() instanceof man52)
+            {
+                Greenfoot.setWorld(new man51());
+            }
         }
         
     }
--- man43.java
+++ man43.java
@@ -17,5 +17,18 @@ public class man43 extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1280, 720, 1); 
+        prepare();
+    }
+
+    /**
+     * El método que permite colocar las flechas en la pantalla cuando
+     * se genera la página del manual.
+     */
+    private void prepare()
+    {
+        l l = new l();
+        addObject(l,67,652);
+        r r = new r();
+        addObject(r,1220,660);
     }
 }
