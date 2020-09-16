aa4b5ae89d8564bb9d1558403511122ab617f81b
Cristian Guillermo Serrano Acevedo <cristiangserrano@gmail.com>
2020-09-15 19:55:39 -0500

    Agregados los demás niveles prácticos

---
+++ test4P.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class test4P here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class test4P extends World
+{
+
+    /**
+     * Constructor for objects of class test4P.
+     * 
+     */
+    public test4P()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
