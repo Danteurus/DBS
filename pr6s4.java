f972ba0d214626f5f44209cd909a7601f57fc21e
Jean C <jeankporti17@gmail.com>
2020-09-16 20:55:17 -0500

    mini cambios

--- pr6s4.java
+++ pr6s4.java
@@ -16,6 +16,6 @@ public class pr6s4 extends World
     public pr6s4()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(1280, 720, 1);  
     }
 }
--- pr7s3.java
+++ pr7s3.java
@@ -16,6 +16,6 @@ public class pr7s3 extends World
     public pr7s3()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(1280, 720, 1); 
     }
 }
