b3d8d73d3593c73dc8de240677600b5cf5e524cb
Jean C <jeankporti17@gmail.com>
2020-09-16 20:30:57 -0500

    preguntas agregadas

---
+++ pr5s4.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class pr5s4 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class pr5s4 extends World
+{
+
+    /**
+     * Constructor for objects of class pr5s4.
+     * 
+     */
+    public pr5s4()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
